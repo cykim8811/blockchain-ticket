d9e4df67106db4bf7f4f08620955529308f8c9a7
cykim <cykim8811@snu.ac.kr>
2025-12-09 19:00:52 +0900

    ssdfsad

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,29 +1,32 @@
 export const events = [
-    {
-        id: 1,
-        title: "Tech Summit 2025",
-        date: "2025-12-15",
-        location: "Seoul, Korea",
-        price: "$150",
-        image: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?auto=format&fit=crop&w=800&q=80",
-        totalSeats: 2
-    },
-    {
-        id: 2,
-        title: "Music Festival 2026",
-        date: "2026-01-20",
-        location: "Tokyo, Japan",
-        price: "$200",
-        image: "https://images.unsplash.com/photo-1591115765373-5207764f72e7?auto=format&fit=crop&w=800&q=80",
-        totalSeats: 40
-    },
-    {
-        id: 3,
-        title: "Modern Art Exhibition",
-        date: "2026-02-10",
-        location: "New York, USA",
-        price: "$50",
-        image: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?auto=format&fit=crop&w=800&q=80",
-        totalSeats: 40
-    }
+  {
+    id: 1,
+    title: "Tech Summit 2025",
+    date: "2025-12-15",
+    location: "Seoul, Korea",
+    price: "$150",
+    image:
+      "https://images.unsplash.com/photo-1540575467063-178a50c2df87?auto=format&fit=crop&w=800&q=80",
+    totalSeats: 10,
+  },
+  {
+    id: 2,
+    title: "Music Festival 2026",
+    date: "2026-01-20",
+    location: "Tokyo, Japan",
+    price: "$200",
+    image:
+      "https://images.unsplash.com/photo-1591115765373-5207764f72e7?auto=format&fit=crop&w=800&q=80",
+    totalSeats: 40,
+  },
+  {
+    id: 3,
+    title: "Modern Art Exhibition",
+    date: "2026-02-10",
+    location: "New York, USA",
+    price: "$50",
+    image:
+      "https://images.unsplash.com/photo-1561214115-f2f134cc4912?auto=format&fit=crop&w=800&q=80",
+    totalSeats: 40,
+  },
 ];
